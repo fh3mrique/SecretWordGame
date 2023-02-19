033aef58dd9be0a0931fbfd1ab807e52ce9e0525
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-02-19 17:42:00 -0300

    Criando componentes do jogo

--- jogo/src/App.js
+++ jogo/src/App.js
@@ -1,11 +1,34 @@
 
+//CSS
 import './App.css';
+
+//React
+import {useCallback, useEffect, useState} from "react";
+
+//dados
+import {palavraLista} from "./data/palavras";
+
+//Componentes
 import TelaInicial from './components/TelaInicial';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages =[
+  {id:1, nome: "start"},
+  {id:2, nome: "game"},
+  {id:3, nome: "end"},
+]
 
 function App() {
+  const [gameInicio, setGameInicio] = useState(stages[0].nome);
+  const [palavras] = useState(palavraLista)
+
+  console.log(palavras)
   return (
     <div className="App">
-      <TelaInicial/>
+      {gameInicio === 'start' && <TelaInicial/>}
+      {gameInicio === 'game' && <Game/>}
+      {gameInicio === 'end' && <GameOver/>}
     </div>
   );
 }
---
+++ jogo/src/components/GameOver.js
@@ -0,0 +1,9 @@
+import './GameOver.css';
+
+function GameOver() {
+  return (
+    <div>GameOver</div>
+  )
+}
+
+export default GameOver
